c4d56afdad5772ddddc27d17291b397d8f6b9206
BradyBurns028 <btb032904@gmail.com>
2025-10-10 15:21:44 -0500

    htf import eloquent

--- backend/app/Models/Faculty.php
+++ backend/app/Models/Faculty.php
@@ -9,6 +9,9 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 /**
  * Class Faculty
