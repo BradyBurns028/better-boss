696af7e227c6e2fd61f3759a1de108359860839d
Mephisto-22 <barfieldc22@gmail.com>
2025-10-10 14:54:40 -0500

    Add default route with Sanctum authentication

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,3 +10,5 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->get('/users', function () {
     return User::all();
 });
+
+Route::middleware('auth:sanctum')->get('/', function () {});
