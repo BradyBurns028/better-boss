59c699424b5b63fdf93e20f4ddb5436505ffc285
tfxler <tfxler@gmail.com>
2025-10-10 09:56:09 -0500

    Created default pages and components

---
+++ frontend/app/components/notifications.vue
@@ -0,0 +1,9 @@
+<!-- notifications box placeholder -->
+<template>
+  <main class="flex flex-col h-full p-2">
+    <div class="h-80 bg-white border border-gray-300 rounded-lg shadow-sm p-3">
+      <h2 class="text-lg font-semibold mb-1">Notifications:</h2>
+      <div class="flex items-center justify-center h-full text-gray-400">No notifications yet</div>
+    </div>
+  </main>
+</template>
