caf024fc190faf62f9ead2e4ff6baedb98167c86
BradyBurns028 <btb032904@gmail.com>
2025-10-16 21:07:35 -0500

    47 comment abstract api filter

--- backend/app/Http/Filters/AbstractApiFilter.php
+++ backend/app/Http/Filters/AbstractApiFilter.php
@@ -4,11 +4,18 @@ namespace App\Http\Filters;
 
 use Illuminate\Http\Request;
 
-abstract class ApiFilter {
+/**
+ * Converts request query params (e.g., ?field[op]=value)
+ * into Eloquent where triplets: [column, operator, value].
+ */
+abstract class AbstractApiFilter {
+    /** @var array<string, list<string>> Allowed params and operators. */
     protected array $safeParms = [];
 
+    /** @var array<string, string> Map request param => DB column. */
     protected array $columnMap = [];
 
+    /** @var array<string, string> Map short ops to SQL ops. */
     protected array $operatorMap = [
         'eq' => '=',
         'lt' => '<',
@@ -18,6 +25,12 @@ abstract class ApiFilter {
         'ne' => '!='
     ];
 
+    /**
+     * Transform request query parameters into Eloquent 'where' triplets.
+     *
+     * @param Request $request Incoming HTTP request carrying query filters.
+     * @return array<int, array{0:string,1:string,2:mixed}> List of [column, operator, value] triplets.
+    */
     public function transform(Request $request): array {
         $eloQuery = [];
 
